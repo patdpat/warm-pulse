1d1044b0f4ca1917dfe0fba1c6c6879a561bc03b
Pat <pat@Pats-MBP.kps.ku.ac.th>
2019-07-12 21:08:26 +0700

    add script and fetch / integrate local storage

--- script/monitor.js
+++ script/monitor.js
@@ -1,1 +1,13 @@
-const baseurl = '"https://exceed.superposition.pknn.dev/data/ace"'
+const baseurl = "https://exceed.superposition.pknn.dev/data/ace";
+fetch(baseurl)
+  .then(res => res.json())
+  .then(data => {
+    console.log(data);
+  });
+
+fetch(baseurl + "/data/ace/door")
+  .then(res => res.text())
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
+
+  
