4f23ea44602318266e7b855d42fbe6cb76c59c0a
Pat <pat@Pats-MBP.kps.ku.ac.th>
2019-07-12 23:59:23 +0700

    add script and fetch / integrate local storage

--- script/monitor.js
+++ script/monitor.js
@@ -3,11 +3,25 @@ fetch(baseurl)
   .then(res => res.json())
   .then(data => {
     console.log(data);
+    document.getElementById("temp").innerText += " " + data["tm"];
+    document.getElementById("hr").innerText += " " + data["hr"];
   });
 
-fetch(baseurl + "/data/ace/door")
-  .then(res => res.text())
-  .then(data => console.log(data))
-  .catch(err => console.log(err));
-
+let brain = JSON.parse(localStorage.getItem("score"));
+console.log(brain);
+// fetch(baseurl + "/data/ace/door")
+//   .then(res => res.text())
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err));
+if (brain <= 9) {
+  document.getElementById("brain").innerText += "normal";
+} else if (brain > 9 && brain <= 18) {
+  document.getElementById("brain").innerText += "concerning";
   
+} else if (brain > 18 && brain <= 27) {
+  document.getElementById("brain").innerText += "";
+  
+} else {
+  document.getElementById("brain").innerText += "critical";
+  //
+}
--- script/question.js
+++ script/question.js
@@ -1,10 +1,12 @@
-let score = 0;
-const checkBoxes = document.getElementsByTagName("input");
-let arr = [].slice.call(checkBoxes);
-const checked = arr.filter(v => v.checked);
-console.log(checkBoxes);
-for (const c of checked) score += Number.parseInt(c.value);
-console.log(score);
-
 // window.location = `low.html?score=${score}`;
-localStorage.setItem("score", JSON.stringify(score));
+function passtomonitor() {
+  let score = 0;
+  const checkBoxes = document.getElementsByTagName("input");
+  let arr = [].slice.call(checkBoxes);
+  const checked = arr.filter(v => v.checked);
+  console.log(checkBoxes);
+  for (const c of checked) score += Number.parseInt(c.value);
+  console.log(score);
+  localStorage.setItem("score", JSON.stringify(score));
+  // The function returns the product of p1 and p2
+}
